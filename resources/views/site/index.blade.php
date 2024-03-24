54426546829e9b3adb5148002cb0066473aeb2b3
Glen Donn B. Alpasan <glendonnalpasan1@gmail.com>
2024-03-24 23:04:42 +0800

    wip

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -12,39 +12,6 @@
 </head>
 
 <body>
-<<<<<<< Updated upstream
-      <div class="container-fluid">
-            <div class="row d-flex">
-                <div class="col-md-8 text-center">
-                    <img src="{{ asset('dist/images/logo.png')}}" width="150" alt="logo">
-                    <h1>Estancia LGU Business and Licensing Permit Information System</h1>
-                </div>
-                <div class="col-md-4 form1 bg-light">
-                    <div class="">
-                        <div class="card-body">
-                            <div class="text-center">
-                                <img src="{{ asset("dist/images/user1.png")}}" width="200" height="200" alt="">
-                            </div>
-                            <form action="" method="POST">
-                                <div>
-                                    <label for="">Username</label>
-                                    <input type="text" class="form-control">
-                                </div>
-                                <div class="mt-4">
-                                    <label for="">Password</label>
-                                    <input type="text" class="form-control">
-                                </div>
-                                {{-- <div class="mt-4">
-                                    <input type="checkbox"><b class="ml-2">Remember Password</b>
-                                </div> --}}
-                                <div class="mt-5">
-                                    <div class="d-grid gap-4">
-                                        <button class="btn btn-primary" type="button">Login</button>
-                                        <button class="btn btn-success" type="button">Sign Up</button>
-                                    </div>
-                                </div>
-                            </form>
-=======
     <div class="container-fluid">
         <div class="row d-flex">
             <div class="col-md-8 text-center">
@@ -57,7 +24,6 @@
                         <div class="text-center">
                             <img src="{{ asset('dist/images/user1.png') }}" width="200" height="200"
                                 alt="">
->>>>>>> Stashed changes
                         </div>
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
