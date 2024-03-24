bb55a4642b0faa145cf1948ee6942052aeaf1ef3
Glen Donn B. Alpasan <glendonnalpasan1@gmail.com>
2024-03-24 23:06:42 +0800

    Update index.blade.php

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -5,14 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <<<<<<< HEAD <title>Document</title>
-        <link rel="stylesheet" href="{{ asset('dist/css/app.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('dist/css/style.css') }}">
-        =======
-        <title>Licensing Permit </title>
-        <link rel="stylesheet" href="{{ asset('dist/css/app.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('dist/css/style.css') }}">
-        >>>>>>> user-registration
+
+    <title>Licensing Permit </title>
+    <link rel="stylesheet" href="{{ asset('dist/css/app.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('dist/css/style.css') }}">
+
 
 </head>
 
