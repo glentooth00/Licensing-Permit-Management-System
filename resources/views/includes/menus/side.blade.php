e199daf6c08e72c5ff17c056f9a761658540f780
Glen Donn B. Alpasan <glendonnalpasan1@gmail.com>
2024-04-16 11:23:08 +0800

    Update side.blade.php

--- resources/views/includes/menus/side.blade.php
+++ resources/views/includes/menus/side.blade.php
@@ -1,96 +1,95 @@
 <div class="pcoded-main-container">
-            <div class="pcoded-wrapper">
-                <nav class="pcoded-navbar">
-                    <div class="sidebar_toggle"><a href="#"><i class="icon-close icons"></i></a></div>
-                    <div class="pcoded-inner-navbar main-menu">
-                        <div class="">
-                            <div class="main-menu-header">
-                                <img class="img-80 img-radius" src="{{ asset('admin/images/faq_man.png')}}" alt="User-Profile-Image">
-                                <div class="user-details">
-                                    <span id="more-details">ADMIN</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="pcoded-navigation-label" data-i18n="nav.category.navigation">MENUS</div>
-                        <ul class="pcoded-item pcoded-left-item">
-                            <li class="">
-                                <a href="#" class="waves-effect waves-dark">
-                                    <span class="pcoded-micon"><i class="ti-home"></i><b>D</b></span>
-                                    <span class="pcoded-mtext">Dashboard</span>
-                                    <span class="pcoded-mcaret"></span>
-                                </a>
-                            </li>
-                        </ul>
-                        <ul class="pcoded-item pcoded-left-item">
-                            <li>
-                                <a href="f#" class="waves-effect waves-dark">
-                                    <span class="pcoded-micon"><i class="ti-user"></i><b>FC</b></span>
-                                    <span class="pcoded-mtext">USERS</span>
-                                    <span class="pcoded-mcaret"></span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/admin/permit" class="waves-effect waves-dark">
-                                    <span class="pcoded-micon"><i class="ti-list"></i><b>FC</b></span>
-                                    <span class="pcoded-mtext">Newly Registered</span>
-                                    <span class="pcoded-mcaret"></span>
-                                </a>
-                            </li>
-                        </ul>
-                        <ul class="pcoded-item pcoded-left-item">
-                            <li>
-                                <a href="f#" class="waves-effect waves-dark">
-                                    <span class="pcoded-micon"><i class="ti-settings"></i><b>FC</b></span>
-                                    <span class="pcoded-mtext">Settings</span>
-                                    <span class="pcoded-mcaret"></span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/admin/permit" class="waves-effect waves-dark">
-                                    <span class="pcoded-micon"><i class="ti-user"></i><b>FC</b></span>
-                                    <span class="pcoded-mtext">Profile</span>
-                                    <span class="pcoded-mcaret"></span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="auth-normal-sign-in.html" class="waves-effect waves-dark">
-                                          <form method="POST" action="{{ route('logout') }}">
-                                            @csrf
-                                            <x-dropdown-link :href="route('logout')"
-                                                onclick="event.preventDefault();
-                                                            this.closest('form').submit();">
-                                                    <span class="pcoded-micon"><i class="ti-layout-sidebar-left"></i><b></b></span>
-                                                    <span class="text-muted" style="margin-left: 20px;"> {{ __('Log Out') }}</span>
-                                                    <span class="pcoded-mcaret"></span>
-                                            </x-dropdown-link>
-                                        </form>
-                                    </span>
-                                </a>
-                            </li>   
-                        </ul>
-                    </div>
-                </nav>
-                <div class="pcoded-content">
-                    <!-- Page-header start -->
-                    <div class="page-header">
-                        <div class="page-block">
-                            <div class="row align-items-center">
-                                <div class="col-md-8">
-                                    <div class="page-header-title">
-                                        <h5 class="m-b-10">Estancia LGU Business </>
-                                        <h5 class="m-b-0">and Licensing Permit Information System </h5>
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <ul class="breadcrumb-title">
-                                        <li class="breadcrumb-item">
-                                            <a href="index.html"> <i class="fa fa-home"></i> </a>
-                                        </li>
-                                        <li class="breadcrumb-item"><a href="#!">Dashboard</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
+    <div class="pcoded-wrapper">
+        <nav class="pcoded-navbar">
+            <div class="sidebar_toggle"><a href="#"><i class="icon-close icons"></i></a></div>
+            <div class="pcoded-inner-navbar main-menu">
+                <div class="">
+                    <div class="main-menu-header">
+                        <img class="img-80 img-radius" src="{{ asset('admin/images/faq_man.png') }}"
+                            alt="User-Profile-Image">
+                        <div class="user-details">
+                            <span id="more-details">ADMIN</span>
                         </div>
                     </div>
-                    <!-- Page-header end -->
+                </div>
+                <div class="pcoded-navigation-label" data-i18n="nav.category.navigation">MENUS</div>
+                <ul class="pcoded-item pcoded-left-item">
+                    <li class="">
+                        <a href="#" class="waves-effect waves-dark">
+                            <span class="pcoded-micon"><i class="ti-home"></i><b>D</b></span>
+                            <span class="pcoded-mtext">Dashboard</span>
+                            <span class="pcoded-mcaret"></span>
+                        </a>
+                    </li>
+                </ul>
+                <ul class="pcoded-item pcoded-left-item">
+                    <li>
+                        <a href="f#" class="waves-effect waves-dark">
+                            <span class="pcoded-micon"><i class="ti-user"></i><b>FC</b></span>
+                            <span class="pcoded-mtext">USERS</span>
+                            <span class="pcoded-mcaret"></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/admin/permit" class="waves-effect waves-dark">
+                            <span class="pcoded-micon"><i class="ti-list"></i><b>FC</b></span>
+                            <span class="pcoded-mtext">Newly Registered</span>
+                            <span class="pcoded-mcaret"></span>
+                        </a>
+                    </li>
+                </ul>
+                <ul class="pcoded-item pcoded-left-item">
+                    <li>
+                        <a href="f#" class="waves-effect waves-dark">
+                            <span class="pcoded-micon"><i class="ti-settings"></i><b>FC</b></span>
+                            <span class="pcoded-mtext">Settings</span>
+                            <span class="pcoded-mcaret"></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/admin/permit" class="waves-effect waves-dark">
+                            <span class="pcoded-micon"><i class="ti-user"></i><b>FC</b></span>
+                            <span class="pcoded-mtext">Profile</span>
+                            <span class="pcoded-mcaret"></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="auth-normal-sign-in.html" class="waves-effect waves-dark">
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <x-dropdown-link :href="route('logout')"
+                                    onclick="event.preventDefault();
+                                            this.closest('form').submit();">
+                                    {{ __('Log Out') }}
+                                </x-dropdown-link>
+                            </form>
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="pcoded-content">
+            <!-- Page-header start -->
+            <div class="page-header">
+                <div class="page-block">
+                    <div class="row align-items-center">
+                        <div class="col-md-8">
+                            <div class="page-header-title">
+                                <h5 class="m-b-10">Estancia LGU Business </>
+                                    <h5 class="m-b-0">and Licensing Permit Information System </h5>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <ul class="breadcrumb-title">
+                                <li class="breadcrumb-item">
+                                    <a href="index.html"> <i class="fa fa-home"></i> </a>
+                                </li>
+                                <li class="breadcrumb-item"><a href="#!">Dashboard</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Page-header end -->
