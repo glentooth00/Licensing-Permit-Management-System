eb232c1eaa8e488079418d7e0616482b4235815c
mantungalvin21 <98724589+mantungalvin21@users.noreply.github.com>
2024-05-15 01:00:45 +0800

    WIP

--- resources/views/includes/menus/top.blade.php
+++ resources/views/includes/menus/top.blade.php
@@ -1,40 +1,36 @@
-<body>
-    <div id="pcoded" class="pcoded">
-        <div class="pcoded-container navbar-wrapper">
-            <nav class="navbar header-navbar pcoded-header">
-                <div class="navbar-wrapper">
-                     <div class="navbar-container container-fluid">
-                    <ul class="nav-right">
-                        <li class="user-profile header-notification">
-                            <a href="#!" class="waves-effect waves-light">
-                                <img src="{{ asset('admin/images/faq_man.png') }}" class="img-radius" alt="User-Profile-Image">
-                                <span>Admin</span>
-                                <i class="ti-angle-down"></i>
-                            </a>
-                            <ul class="show-notification profile-notification">
-                                <li class="waves-effect waves-light">
-                                    <a href="user-profile.html">
-                                        <i class="ti-user"></i> Profile
-                                    </a>
-                                </li>
-                                {{-- <li class="waves-effect waves-light">
-                                    <a href="auth-normal-sign-in.html">
-                                        <i class="ti-layout-sidebar-left"></i> Logout
-                                    </a>
-                                </li> --}}
-                                <li class="waves-effect waves-light">
-                                    {{-- <a href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();"> --}}
-                                        <form method="POST" action="{{ route('logout') }}">
-                                            @csrf
-                                            <a href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
-                                                 <i class="ti-layout-sidebar-left"></i> Logout
-                                            </a>
-                                        </form>
-                                    {{-- </a> --}}
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-             </div>
-        </nav>
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+
+      <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#">
+          <i class="fas fa-cog"></i>
+        </a>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <span class="dropdown-header">Settings</span>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-user mr-2"></i> Profile
+          </a>
+          <div class="dropdown-divider"></div>
+             {{-- <a href="auth-normal-sign-in.html" class="dropdown-item"> --}}
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <a class="dropdown-item" href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
+                        <i class="fas fa-sign-out-alt mr-2"></i> {{ __('Log Out') }}
+                    </a>
+                </form>
+            {{-- </a> --}}
+        </div>
+      </li>
+    </ul>
+  </nav>
+  <!-- /.navbar -->
