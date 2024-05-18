607ed6bb77231c16c9dd4b81d25b1307ee37ce8c
Glen Donn B. Alpasan <glendonnalpasan1@gmail.com>
2024-05-18 11:44:32 +0800

    Update header.blade.php

--- resources/views/includes/sections/header.blade.php
+++ resources/views/includes/sections/header.blade.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Starter</title>
 
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
-  {{-- DATATABLES --}}
-  <link rel="stylesheet" href="/dist/plugins/datatables/css/dataTables.bootstrap4.min.css">
-  <link rel="stylesheet" href="/dist/plugins/datatables/css/responsive.bootstrap4.min.css">
-  <link rel="stylesheet" href="/dist/plugins/datatables/css/buttons.bootstrap4.min.css">
-  <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="/dist/plugins/fontawesome-free/css/all.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="/dist/css/adminlte.min.css">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Starter</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    {{-- DATATABLES --}}
+    <link rel="stylesheet" href="/dist/plugins/datatables/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="/dist/plugins/datatables/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="/dist/plugins/datatables/css/buttons.bootstrap4.min.css">
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="/dist/plugins/fontawesome-free/css/all.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="/dist/css/adminlte.min.css">
 </head>
+
 <body class="hold-transition sidebar-mini">
     <div class="wrapper">
