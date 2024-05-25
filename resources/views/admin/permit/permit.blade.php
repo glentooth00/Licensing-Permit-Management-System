df76df8ba7b17ca0771282b66aa440b06922f977
Glen Donn B. Alpasan <glendonnalpasan1@gmail.com>
2024-05-25 11:00:59 +0800

    Reapply "wip"
    This reverts commit 1e40ae19841c3fb6413c31fe39e71f11efc47d52.

--- resources/views/admin/permit/permit.blade.php
+++ resources/views/admin/permit/permit.blade.php
@@ -22,8 +22,11 @@
         <div class="content">
             <div class="container-fluid">
                 <div class="card">
+
                     <!-- /.card-header -->
                     <div class="card-body">
+                        <button class="btn btn-primary" onclick="printPage()"> <i class="fa-solid fa-print"></i>
+                            Print</button>
                         <div class="text-center">
                             <span>Republic of the Philippines</span><br>
                             <span>Province of Iloilo</span>
@@ -37,6 +40,7 @@
                         <div>
                             <div class="row text-center">
                                 <div class="col-lg-4 mt-5" style="border-top: 1px solid gray;">
+
                                     <span>DATE Issued</span>
                                 </div>
                                 <div class="col-lg-4 mt-5" style="border-top: 1px solid gray;">
@@ -173,5 +177,16 @@
         <!-- /.content -->
     </div>
     <!-- /.content-wrapper -->
-
+    <style>
+        @media print {
+            .btn {
+                display: none;
+            }
+        }
+    </style>
+    <script>
+        function printPage() {
+            window.print();
+        }
+    </script>
 @endsection
